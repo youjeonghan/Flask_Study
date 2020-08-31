f14461de1f16db657b2c52f0159b380122419b7b
youjeonghan <57481424+youjeonghan@users.noreply.github.com>
2020-08-31 12:49:02 +0900

    07. TODO 모델링 (20.08.31)

--- flask_project_fastcampus/app.py
+++ flask_project_fastcampus/app.py
@@ -1,9 +1,25 @@
+import os
 from flask import Flask
+from flask import request
 from api_v1 import api as api_v1
-
+from models import db
 
 app = Flask(__name__)
 app.register_blueprint(api_v1, url_prefix='/api/v1')
+
+basdir = os.path.abspath(os.path.dirname(__file__))
+dbfile = os.path.join(basdir, 'db.sqlite')
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'QWERQEWRQWERQWER'
+
+
+db.init_app(app)		# 초기화
+db.app = app 			# 등록
+db.create_all()			# 반영
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=5000, debug=True)
---
+++ flask_project_fastcampus/models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Todo(db.Model):
+	__tablename__ = 'todo'
+
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(256))
+	tstamp = db.Column(db.DateTime, sever_default = db.func.now())
+
+	@property
+	def serialize(self):
+		return{
+			'id': self.id,
+			'title': self.title,
+			'tstamp': self.tstamp
+		}

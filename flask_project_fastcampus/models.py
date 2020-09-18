e02a75a523a58edf2adf51dc98c1dcb4948b640d
youjeonghan <57481424+youjeonghan@users.noreply.github.com>
2020-09-19 03:11:04 +0900

    11. (복습) 회원가입 MVC 만들기 - 1

--- flask_project_fastcampus/models.py
+++ flask_project_fastcampus/models.py
@@ -6,6 +6,7 @@ class Todo(db.Model):
 	__tablename__ = 'todo'
 
 	id = db.Column(db.Integer, primary_key=True)
+	fcuser_id = db.Column(db.Integer, db.ForeignKey('fcuser.id'), nullable = False)
 	title = db.Column(db.String(256))
 	tstamp = db.Column(db.DateTime, server_default = db.func.now())
 
@@ -16,3 +17,10 @@ class Todo(db.Model):
 			'title': self.title,
 			'tstamp': self.tstamp
 		}
+
+class Fcuser(db.Model):
+	__tablename__ = 'fcuser'
+	id = db.Column(db.Integer, primary_key=True)
+	userid = db.Column(db.String(32))
+	password = db.Column(db.String(128))
+	todos = db.relationship('Todo', backref='fcuser', lazy=True)		# 나를 'Todo'라는 객체가 가져갈때 fcuser로 등록하겠다는뜻 	# lazy=True는 데이터베이스에서 가져올때 로드를 하겠다는 뜻

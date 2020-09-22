aaa64520b12f177fff89bdc340cdd493d4f68ea3
youjeonghan <57481424+youjeonghan@users.noreply.github.com>
2020-09-23 02:24:05 +0900

    14. TODO CRUD 만들기 - 1

--- flask_project_fastcampus/api_v1/todo.py
+++ flask_project_fastcampus/api_v1/todo.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from flask import request
 from flask import Blueprint
+from flask import session
 from models import Todo, db
 import datetime
 import requests
@@ -10,17 +11,33 @@ def send_slack(msg):
 	res = requests.post('https://hooks.slack.com/services/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',json={\
 		'text': msg}, headers={'Content-Type':'application/json'})
 
-@api.route('/todos',methods=['GET','POST'])
+@api.route('/todos',methods=['GET','POST', 'DELETE'])
 def todos():
 	if request.method == 'POST':
-		# 생성하는 코드가 추가됨
-		send_slack('TODO가 생성되었습니다')		# 사용자 정보, 할일 제목, 기한
+		userid = session.get('userid', None)
+		if not userid:
+			return jsonify(), 401
 
+		data = request.get_json()
+
+		todo = Todo()
+		todo.title = data.get('title')
+		todo.fcuser_id = userid
+
+		db.session.add(todo)
+		db.session.commit()
+
+
+		send_slack('TODO가 생성되었습니다')		# 사용자 정보, 할일 제목, 기한
+		return jsonify(), 201
 
 	elif request.method == 'GET':
 		pass
 
-	data = request.get_json()
+	elif request.method == 'DELETE':
+		pass
+
+	
 	return jsonify(data)
 
 
@@ -46,6 +63,6 @@ def slack_todos():
 	elif cmd == 'list':
 		todos = Todo.query.all()
 		for idx, todo in enumerate(todos):
-			ret_msg += '%d. %s (~ %s)\n' % (idx+1, todo.title, str(todo.tstamp))
+			ret_msg += '%d. %s (~ %s)\n' % (idx + 1, todo.title, str(todo.tstamp))
 		
 	return ret_msg
